79c57047c31cece4b4c65360f4ec17af1268b097
Mathieu CAROFF <mathieu4f@users.noreply.github.com>
2015-03-30 19:10:59 +0200

    Modification de CubeClass.py
    Ajouts de commentaires, et poursuite de l'écriture des fonctions d'identification des arêtes et des sommets.

--- CubeClass.py
+++ CubeClass.py
@@ -29,22 +29,34 @@ class Cube:
     def __init__ (s, chaineU):
         s.faces = chaineU.split(',')
         # La facette n°4 (le centre) définit la couleur de la face :
-        s.couleurs = [ face[4] for face in s.faces ]
-        (s.W, s.B, s.O, s.V, s.R, s.J) = s.couleurs
-        (s.Wf, s.Bf, s.Of, s.Vf, s.Rf, s.Jf) = s.faces
-        s.sommets = [() for huit in range(8)]
-        s.aretes = [() for douze in range(12)]
-        s.couronneHaut = ''.join([ strg[0:3] for strg in s.faces[1:5]])
-        s.couronneMil  = ''.join([ strg[3:6] for strg in s.faces[1:5]])
+        s.couleurs = [ face[4] for face in s.faces ] # Le quatrième caractère est le centre de la face.
+        (s.W, s.B, s.O, s.V, s.R, s.J) = s.couleurs # Nous affectons des noms spécifique à chaque couleur.
+        (s.Wf, s.Bf, s.Of, s.Vf, s.Rf, s.Jf) = s.faces # Nous affectons aussi des nom à chaque face.
+        s.sommets = [() for huit in range(8)] # Nous pré-créons ces huit valeur, pour pouvoir les affecter
+        s.aretes = [() for douze in range(12)] # plus facilement ensuite, en utilisant les indices 'L[i]' uniquement.
+        # Et pas 'L.append'
+        s.couronneHaut = ''.join([ strg[0:3] for strg in s.faces[1:5]]) # On récupère les couronnes à différentes hauteurs
+        s.couronneMil  = ''.join([ strg[3:6] for strg in s.faces[1:5]]) # Ceci facilitera l'identification ensuite.
         s.couronneBas  = ''.join([ strg[6:9] for strg in s.faces[1:5]])
         Wf,Jf = s.Wf, s.Jf
+        # Notion d'anneau (haut et bas) coresspond à une lecture particulière des faces :
+        # 5 4 3
+        # 6 . 2 - Face du haut, numérotation de l'anneau
+        # 7 0 1
+        # . . .
+        # . . .
+        # . . .
+        # 7 0 1
+        # 6 . 2 - Face du bas, numérotation de l'anneu (aussi)
+        # 5 4 3
+        # Code correspondant:
         s.anneauHaut   = ''.join([Wf[7:9],Wf[5],Wf[3:0:-1],Wf[3],Wf[6]])
         s.anneauBas    = ''.join([Jf[5],Jf[9:6:-1],Jf[3],Jf[0:3]])
-        s.modeleResolu = genModeleResolu(s.couleurs)
-        s.modeleA = genModeleA(s.couleurs)
+        s.modeleResolu = genModeleResolu(s.couleurs) # Nous intégrons deux modèles, pour les testes
+        s.modeleA = genModeleA(s.couleurs) # ~
 
     
-    valeurParCouleur = {W:0, B:0, O:0, J:1, V:2, R:4}
+    valeurParCouleurSommet = {W:0, B:0, O:0, J:1, V:2, R:4}
     # [W-J:Axe 0: 1], [B-V:Axe 1: 2], [O-R:Axe 2: 4]
     
     def identifieSommet (s,bloc3f):
@@ -52,33 +64,34 @@ class Cube:
         # bloc3f contient trois couleurs: trois facettes
         # Identifions la rotation du sommet:
         rotation = 3
-        for (i,x) in enumerate(bloc3f): 
+        for i,x in enumerate(bloc3f): 
             if x == s.W or x == s.J:
                 rotation = i
         if rotation == 3:
             return "Erreur de sommet dans la description du cube"
         # Calculons le numero (entre 0 et 7) du sommet:
-        num = sum([ valeurParCouleur[x]  for x in bloc3f ])
-            
-
+        num = sum([ valeurParCouleurSommet[x]  for x in bloc3f ])
+        return (num,rotation)
+        
+    valeurParCouleurArete = {W:0,J:4,B:0,V:1,O:2,R:3} # A définir !
     def identifieArete (s,bloc2f):
         """ Caractérise une arête du cube, à partir de deux couleurs d'un bloc."""
         # bloc2f contient deux couleurs: deux facettes
         # Identifions la rotation de l'arête:
         rotation == 2
-        
-        for (i,x) in enumerate(bloc2f):
+        for i,x in enumerate(bloc2f):
             if x == s.W or x == s.J: # Verification de la couleur de la facette.
                 rotation = i # Si une des deux facettes correspond, on le retient.
-                
         if rotation == 2: # Si on a pas pu determiner la rotation, on utilise le bleu-vert:
-            for (i,x) in enumerate(bloc2f):
+            for i,x in enumerate(bloc2f):
                 if x == s.B or x == s.V:
                     rotation = i
-                    
         if rotation == 2:
             return "Erreur d'arête dans la description du cube"
-
+        # Identifions maintenant le numero du bloc.
+        # Le systeme d'identification ci-dessous est insuffisant. Il ne produit pas un nombre entre 0 et 11.
+        num = sum([ valeurParCouleurArete[x] for x in bloc2f ])
+        return (num,rotation)
 
 
 # Le regroupement des sommets et des aretes:
@@ -104,7 +117,8 @@ class Cube:
 #                   3 . 1 #                   . 9 . 
 # [J:1, V:2, R:4]
 
-    def groupSommets (s):        
+    def groupSommets (s):
+        """Regroupe les facettes des sommets par 2, selon leur numéro, en vue de leur identification"""
         # On s'occupe des couches Haut et Bat en même temps. 
         for i,x in enumerate([0,1,3,2]): # Pour chaque sommet...
             s.sommets[2*i]   = (s.anneauHaut[2*x],
@@ -115,9 +129,12 @@ class Cube:
                                  s.couronneBas[(3*x+2)%12])
 
     def groupAretes (s):
+        """Regroupe les facettes des arêtes par 2, selon leur numéro, en vue de leur identification"""
+        # Les quatres premières et quatre dernières arêtes de la liste peuvent s'identifier ainsi :
         for x in range(4):
             s.aretes[x]   = (s.anneauHaut[2*x],s.couronneHaut[3*x+1])
             s.aretes[8+x] = (s.anneauBas[2*x],s.couronneBas[3*x+1])
+        # Les arêtes suivantes sont identifiées manuellement :
         s.aretes[4] = tuple(s.couronneMil[2:4])
         s.aretes[5] = (s.couronneMil[6],s.couronneMil[5])
         s.aretes[6] = tuple(s.couronneMil[8:10])
